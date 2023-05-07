2758acbc6f8db1de85c6ebd9d44a314c37af99a6
ismael M <ismaelmontelongomon@gmail.com>
2023-05-07 23:53:53 +0100

    cambio de puerto

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(443, () => {
+    console.log('listening on *:443');
 });
 
 // funciones
